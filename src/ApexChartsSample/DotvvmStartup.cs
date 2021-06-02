549df8e708ec2a8013129d6882e5d8d980d50fab
Tomáš Herceg <tomas.herceg@riganti.cz>
2021-06-02 15:17:45 +0200

    Initial version added

---
+++ src/ApexChartsSample/DotvvmStartup.cs
@@ -0,0 +1,55 @@
+﻿using DotVVM.Framework.Configuration;
+using DotVVM.Framework.ResourceManagement;
+using DotVVM.Framework.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApexChartsSample
+{
+    public class DotvvmStartup : IDotvvmStartup, IDotvvmServiceConfigurator
+    {
+        // For more information about this class, visit https://dotvvm.com/docs/tutorials/basics-project-structure
+        public void Configure(DotvvmConfiguration config, string applicationPath)
+        {
+
+            ConfigureRoutes(config, applicationPath);
+            ConfigureControls(config, applicationPath);
+            ConfigureStylesheets(config, applicationPath);
+            ConfigureScripts(config, applicationPath);
+        }
+
+        private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath)
+        {
+            config.RouteTable.Add("Default", "", "Views/Default.dothtml");
+            config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config));
+        }
+
+        private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
+        {
+        }
+
+        private void ConfigureScripts(DotvvmConfiguration config, string applicationPath)
+        {
+            config.Resources.Register("apex-charts-js", new ScriptResource()
+            {
+                Location = new UrlResourceLocation("~/ApexCharts/apexcharts.min.js")
+            });
+        }
+
+        private void ConfigureStylesheets(DotvvmConfiguration config, string applicationPath)
+        {
+            config.Resources.Register("style-css", new StylesheetResource()
+            {
+                Location = new UrlResourceLocation("~/Resources/style.css")
+            });
+            config.Resources.Register("apex-charts-css", new StylesheetResource()
+            {
+                Location = new UrlResourceLocation("~/ApexCharts/apexcharts.css")
+            });
+        }
+
+        public void ConfigureServices(IDotvvmServiceCollection options)
+        {
+            options.AddDefaultTempStorages("temp");
+        }
+    }
+}
---
+++ src/ApexChartsSample/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using DotVVM.Framework.Hosting;
+
+namespace ApexChartsSample.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+        public string Title { get; set; }
+
+        public DefaultViewModel()
+        {
+            Title = "DotVVM ♥ Apex Charts";
+        }
+
+        public int[] AreaData1 { get; set; } = { 31, 40, 28, 51, 42, 109, 100 };
+        public int[] AreaData2 { get; set; } = { 11, 32, 45, 32, 34, 52, 41 };
+
+        public string[] Teams { get; set; } = { "Team A", "Team B", "Team C", "Team D", "Team E" };
+        public int[] TeamsData { get; set; } = { 11, 32, 45, 32, 34, 52, 41 };
+        public string[] Countries { get; set; } = { "South Korea", "Canada", "United Kingdom", "Netherlands", "Italy", "France", "Japan",
+            "United States", "China", "Germany"};
+        public int[] CountriesData { get; set; } = { 400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380 };
+    }
+}
